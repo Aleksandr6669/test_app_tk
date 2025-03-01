9d3b55409029684020d7ab59fa6bc9e1150f1dac
Aleksandr6669 <50256477+Aleksandr6669@users.noreply.github.com>
2025-03-01 11:15:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Простое приложение на Flet"
 
-    
+    # Добавляем шапку с названием приложения
+    page.add(
+        ft.AppBar(
+            title=ft.Text("Простое приложение на Flet"),
+            center_title=True
+        )
+    )
 
 ft.app(target=main)
